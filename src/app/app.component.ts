5d1e9714e5385b1aa4529035fe7aee23b1ab0c5b
ashish-panicker <ashish.s.panicker@gmail.com>
2024-03-11 10:27:06 +0530

    Angular app with - Tailwind CSS - Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomeComponent } from './shared/home/home.component';
+import { ListUsersComponent } from './users/list-users/list-users.component';
+import { NewUserComponent } from './users/new-user/new-user.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NavbarComponent, HomeComponent, ListUsersComponent, NewUserComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'users-app';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './shared/home/home.component';
+import { ListUsersComponent } from './users/list-users/list-users.component';
+import { NewUserComponent } from './users/new-user/new-user.component';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+/**
+ * Each route comprises of a path and a component that is mapped for that path.
+ * Example:
+ * https://angular.io/guide/router
+ * My Application URL: http://localhost:4200
+ */
+export const routes: Routes = [
+    // http://localhost:4200/home
+    { path: 'home', component: HomeComponent },
+    // http://localhost:4200/list-user
+    { path: 'list-user', component: ListUsersComponent },
+    // http://localhost:4200/add-user
+    { path: 'create-user', component: NewUserComponent },
+    // http://localhost:4200
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    // http://localhost:4200/**
+    { path: '**', component: PageNotFoundComponent },
+];
